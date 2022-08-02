42f47a6ec7be08e23fe58c5ef9ca87e1beb45d59
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-02 13:48:17 -0700

    Breadth First

--- 010-Binary-Tree-Traversal.cpp
+++ 010-Binary-Tree-Traversal.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
-
 /**
  * @brief Tree Traversal
  * process of visiting each node in the tree exactly once in some order
@@ -11,10 +11,44 @@ using namespace std;
  * Depth-first: Traverse by 
  */
 
-int main() {
+/**
+ * @brief Defining a Node from a Binary Tree 
+ * 
+ * Level Order - Set a Pointer name current and keep moving it 
+ */
 
+struct Node {
+    char data;
+    Node *left;
+    Node *right;
+}; 
+
+// Pass address of the root of argument
+void LevelOrder(Node* root) {
+
+    // Tree is empty
+    if (root == NULL) return;
     
+    // Create a Queue
+    queue<Node*> Q;
+    Q.push(root); // Insert the address of the root node
 
+    // While there is at least one discovered node
+    while(!Q.empty()) {
+        // Take a node from the front, as current pointer 
+        Node* current = Q.front();
+        cout << current -> data << " ";
+
+        // left child not null, push address into the queue
+        if (current -> left != NULL) Q.push(current -> left);
+        if (current -> right != NULL) Q.push(current -> right);
+        Q.pop(); // removing element at front
+    } 
+}
+
+int main() { 
+
+    // To test Level Order function
 
 
 }
