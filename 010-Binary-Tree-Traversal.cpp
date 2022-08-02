879d36c51e4fccd5e9343fe985feb7b57d3a8eda
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-01 23:57:09 -0700

    BST

---
+++ 010-Binary-Tree-Traversal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Tree Traversal
+ * process of visiting each node in the tree exactly once in some order
+ * 
+ * visit -> means to read/process data in a node
+ * B-first: Traverse by level
+ * Depth-first: Traverse by 
+ */
+
+int main() {
+
+    
+
+
+
+}
+
+
+
+
+
+
+
